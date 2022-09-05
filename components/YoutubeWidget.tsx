5a6ddbc76d27367b9e78cc3fa280e1d9a116deb5
Jonathan Brink <jonathandavidbrink@gmail.com>
2022-09-05 15:12:10 -0400

    youtube fix

--- components/YoutubeWidget.tsx
+++ components/YoutubeWidget.tsx
@@ -1,14 +1,20 @@
+import { useEffect, useState } from "react";
 import data from "../data/youtube_raw.json";
 
+const oneDay = 1000 * 60 * 60 * 24;
+const dayOfYear = () => {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = now.valueOf() - start.valueOf();
+  return Math.floor(diff / oneDay);
+};
+
 const YoutubeWidget = () => {
-  const oneDay = 1000 * 60 * 60 * 24;
-  const dayOfYear = () => {
-    const now = new Date();
-    const start = new Date(now.getFullYear(), 0, 0);
-    const diff = now.valueOf() - start.valueOf();
-    return Math.floor(diff / oneDay);
-  };
-  const youtubeHash = data[dayOfYear() % data.length];
+  const [youtubeHash, setYoutubeHash] = useState("");
+  useEffect(() => {
+    const hash = data[dayOfYear() % data.length];
+    setYoutubeHash(hash);
+  }, []);
   return (
     <div
       className="youtube"
