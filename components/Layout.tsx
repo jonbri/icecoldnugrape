2922dd19ad3f01a791262b8e490526b1d8c915a4
Jonathan Brink <jonathandavidbrink@gmail.com>
2024-04-03 16:36:40 -0400

    favicon uses Link
    Fixes #40

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -58,7 +58,7 @@ const Layout = ({ children, type, prev, next }: LayoutProps) => {
         <title>icecoldnugrape</title>
         <meta name="og:title" content="icecoldnugrape" />
         <meta name="description" content="icecoldnugrape" />
-        <link rel="icon" href="/favicon.ico" />
+        <Link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={cn.header}>
         <div className={cn.h1Container}>
