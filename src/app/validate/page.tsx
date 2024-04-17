b48fb809cf69d99e3052a3359d670cf05644c067
Jonathan Brink <jonathandavidbrink@gmail.com>
2024-04-17 16:27:05 -0400

    more validate

--- src/app/validate/page.tsx
+++ src/app/validate/page.tsx
@@ -1,6 +1,9 @@
 import { getRecordings } from "../../data";
 import Link from "next/link";
 
+const unique = <T,>(value: T, index: number, array: T[]) =>
+  array.indexOf(value) === index;
+
 interface Error {
   href: string;
   message: string;
@@ -8,12 +11,30 @@ interface Error {
 export default function Page() {
   const errors: Error[] = [];
   const recordings = getRecordings();
-  recordings.forEach(({ id, type, name, formattedTitle }) => {
+  recordings.forEach(({ id, type, name, jon, songs, formattedTitle }) => {
     if (type === "Album" || type === "Single") {
       if (name === undefined) {
         errors.push({
           href: `/recordings/${id}`,
-          message: `${formattedTitle} is missing a name`,
+          message: `"${formattedTitle}" is missing a name`,
+        });
+      }
+      if (jon !== undefined) {
+        errors.push({
+          href: `/recordings/${id}`,
+          message: `"${formattedTitle}" cannot be marked with "jon"`,
+        });
+      }
+    }
+
+    // check for duplicate "n" field
+    if (songs) {
+      const n = songs.map(({ n }) => n.toString());
+      const uniqueN = n.filter(unique);
+      if (n.length !== uniqueN.length) {
+        errors.push({
+          href: `/recordings/${id}`,
+          message: `"${formattedTitle}" has duplicate "n" fields`,
         });
       }
     }
